8fedc342e95e64cfc906b187288ee253861bb0d0
Bart Bronselaer <bart.bronselaer@me.com>
2018-11-27 18:26:31 +0100

    Initial Commit

---
+++ TwitterTrial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TwitterTrial
+//
+//  Created by Bart Bronselaer on 27/11/2018.
+//  Copyright © 2018 Bart Bronselaer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
